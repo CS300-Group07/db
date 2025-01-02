a721a55511dcd75bc2f552eb9e310f2f83d9733b
tungvh04 <116733524+tungvh04@users.noreply.github.com>
2025-01-02 13:18:37 +0700

    Update create_database.sql

--- init/create_database.sql
+++ init/create_database.sql
@@ -27,7 +27,7 @@ CREATE TABLE products (
     product_id SERIAL PRIMARY KEY,
     name VARCHAR(100) NOT NULL,
     description TEXT,
-    price NUMERIC(10, 2) NOT NULL,
+    price NUMERIC(20, 2) NOT NULL,
     stock_quantity INT DEFAULT 0,
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
     updated_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP,
